aca61093175bee6cc8e63c3252de6d352adc2b18
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-09-01 13:07:43 +0200

    feat(schemas): add pydantic schema for User model

---
+++ app/schemas/user.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+class UserRead(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    is_active: bool
+
+    class Config:
+        from_attributes = True
