91f0281f6aae25fadea38c3bb954815db6ad6d97
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-09-06 18:35:35 +0200

    feat(app): register auth routes for login and me endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-from ClubConnect.app.routers import club
+
+from ClubConnect.app.auth.routes import router as auth_router
+from ClubConnect.app.routers import club, user
 
 app = FastAPI(title="ClubTrack API")
 app.include_router(club.router)
+app.include_router(auth_router)
+#app.include_router(user.router)
 
 
 # to run: python -m uvicorn ClubConnect.app.main:app --reload
