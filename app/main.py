13f762a016f8d305d02407296a84ca7ab655a93c
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-09-10 15:40:28 +0200

    feat(main): include user router to main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from ClubConnect.app.routers import club, user
 app = FastAPI(title="ClubTrack API")
 app.include_router(club.router)
 app.include_router(auth_router)
-#app.include_router(user.router)
+app.include_router(user.router)
 
 
 # to run: python -m uvicorn ClubConnect.app.main:app --reload
