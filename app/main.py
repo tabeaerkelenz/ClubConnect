3ba6177d15c575502ebd7b66c40391f857613005
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-09-11 20:14:47 +0200

    fix(main): change the include_router names to plural as the users and clubs routers

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
 from ClubConnect.app.auth.routes import router as auth_router
-from ClubConnect.app.routers import club, user
+from ClubConnect.app.routers import clubs, users
 
 app = FastAPI(title="ClubTrack API")
-app.include_router(club.router)
+app.include_router(clubs.router)
 app.include_router(auth_router)
-app.include_router(user.router)
+app.include_router(users.router)
 
 
 # to run: python -m uvicorn ClubConnect.app.main:app --reload
