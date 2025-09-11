b4551d754160edb91334e6b2520e102207a80a5a
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-09-11 15:19:31 +0200

    feat(clubs): restrict auth to delete/patch; make read endpoints public

--- app/routers/club.py
+++ app/routers/club.py
@@ -13,7 +13,6 @@ from ClubConnect.app.auth.deps import get_current_active_user
 router = APIRouter(
     prefix="/clubs",
     tags=["clubs"],
-    dependencies=[Depends(get_current_active_user)]
 )
 
 
@@ -47,7 +46,7 @@ def get_club_endpoint(club_id: int, db: Session = Depends(get_db)):
 def get_clubs_endpoint(skip: int = 0, limit: int = Query(50, le=200), q: Optional[str] = Query(None, description="search by club name"), db: Session = Depends(get_db)):
     return list_clubs(db, skip=skip, limit=limit, q=q)
 
-@router.patch("/{club_id}", response_model=ClubRead, status_code=status.HTTP_200_OK)
+@router.patch("/{club_id}", response_model=ClubRead, status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_active_user)])           # add dependency get current active user
 def update_club_endpoint(club_id: int, payload: ClubUpdate, db: Session = Depends(get_db)):
     try:
         club = update_club(db, club_id, payload)
@@ -57,7 +56,7 @@ def update_club_endpoint(club_id: int, payload: ClubUpdate, db: Session = Depend
         raise HTTPException(status_code=404, detail="Club not found")
     return club
 
-@router.delete("/{club_id}", response_model=ClubRead, status_code=status.HTTP_200_OK)
+@router.delete("/{club_id}", response_model=ClubRead, status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_active_user)])      # add dependency get current active user
 def delete_club_endpoint(club_id: int, db: Session = Depends(get_db)):
     club = delete_club(db, club_id)
     if not club:
