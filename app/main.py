fd0651ca4e60f6e36018b1a23c42e790c66dbf56
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-09-14 20:38:37 +0200

    feat(main): include session endpoint and change naming of membership routes for clear naming

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 
 from ClubConnect.app.auth.routes import router as auth_router
-from ClubConnect.app.routers import clubs, users, memberships
+from ClubConnect.app.routers import clubs, users, memberships, plans, sessions
 
 app = FastAPI(title="ClubTrack API")
 app.include_router(clubs.router)
 app.include_router(auth_router)
 app.include_router(users.router)
-app.include_router(memberships.router)
+app.include_router(memberships.clubs_memberships_router)    # specify the router name
+app.include_router(memberships.memberships_router)
+app.include_router(plans.router)
+app.include_router(sessions.router)
 
 # to run: python -m uvicorn ClubConnect.app.main:app --reload
