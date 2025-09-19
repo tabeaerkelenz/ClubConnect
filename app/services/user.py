06fe52e126f735b1dc3b8c7e2266a994ec20ab65
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-09-19 23:08:38 +0200

    refractor: refractor users logic in service and crud layer to follow FastAPI best practice

---
+++ app/services/user.py
@@ -0,0 +1,8 @@
+# python
+from sqlalchemy.orm import Session
+from ClubConnect.app.crud.user import get_user_by_email, create_user
+
+def create_user_service(db: Session, name: str, email: str, password: str):
+    if get_user_by_email(db, email):
+        raise ValueError("Email already registered")
+    return create_user(db, name=name, email=email, password=password)
