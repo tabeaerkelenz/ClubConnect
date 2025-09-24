15d03cf7e302e68921b77513198e093d834f3864
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-09-24 16:06:28 +0200

    fix: linearize alembic chain (stub 8f5b7b8c9726 -> baseline 8bdceb080455), drop __pycache__

--- alembic/versions/8f5b7b8c9726_stub_legacy_baseline_id.py
+++ alembic/versions/8f5b7b8c9726_stub_legacy_baseline_id.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '8f5b7b8c9726'
-down_revision: Union[str, Sequence[str], None] = '8bdceb080455'
+down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
